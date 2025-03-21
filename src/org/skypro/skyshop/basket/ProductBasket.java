7df2cd278b8c6d3073f497c7bcc1d5edd88d2418
TrueRandolf <randolf@yandex.ru>
2025-03-21 14:47:26 +0300

    Курс 2 Домашнее задание №2

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,61 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private final Product[] innerBasket = new Product[5];
+
+    public ProductBasket() {
+    }
+
+    public void addProductToBasket(Product product) {
+        for (int i = 0; i < innerBasket.length; i++) {
+            if (innerBasket[i] == null) {
+                innerBasket[i] = product;
+                return;
+            }
+        }
+        System.out.println("Невозможно добавить продукт");
+    }
+
+    public long getSumPrice() {
+        long summ = 0;
+        for (Product prod : innerBasket) {
+            if (prod != null) {
+                summ += prod.getPrice();
+            }
+        }
+        return summ;
+    }
+
+    public void printBasket() {
+        boolean nEmpty = false;
+        for (Product prod : innerBasket) {
+            if (prod != null) {
+                System.out.println(prod);
+                nEmpty = true;
+            }
+        }
+        if (nEmpty) {
+            System.out.println("Итого:<" + getSumPrice() + ">");
+            return;
+        }
+        System.out.println("в корзине пусто");
+    }
+
+    public boolean getIsProductInBasket(Product product) {
+        for (Product prod : innerBasket) {
+            if (prod != null && prod.equals(product)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearBasket() {
+        for (int i = 0; i < innerBasket.length; i++) {
+            innerBasket[i] = null;
+        }
+
+    }
+}
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,30 @@
+package org.skypro.skyshop.product;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + name + ">:<" + price + '>';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+}
+
