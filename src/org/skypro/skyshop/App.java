fc89252988a5aaa6919ee779d061b1294fb00ef7
TrueRandolf <randolf@yandex.ru>
2025-03-31 20:46:35 +0300

    Курс 2 Домашнее задание №6 Java Collection Framework: Map

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
 
         try {
-            productBasketDemo("Гвоздь");
+            productBasketDemo("Болт");
         } catch (IllegalArgumentException e) {
             System.out.println(e);
             System.out.println();
@@ -21,50 +21,51 @@ public class App {
         try {
             searchEngineDemo("Болт2");
             searchEngineDemo("Гвоздь");
+            searchEngineDemo("Болт");
         } catch (BestResultNotFound e) {
             System.out.println("e = " + e);
         }
-
     }
 
     public static void searchEngineDemo(String searchString) {
         SearchEngine thatStorage = new SearchEngine();
         System.out.println(">РАБОТА С SEARCHENGINE:");
+        thatStorage.printAll();
         System.out.println(">добавление элементов метод addNewItem :");
         thatStorage.addNewItem(new FixPriceProduct("Глухарь"));
         thatStorage.addNewItem(new DiscountProduct("Рымболт", 500, 5));
-        //Оставлено для проверки! Не комментарии!
-        //thatStorage.addNewItem(new SimpleProduct("Шайба", 100));
-        //thatStorage.addNewItem(new FixPriceProduct("Саморез"));
-        //thatStorage.addNewItem(new DiscountProduct("Винт", 100, 10));
-        //thatStorage.addNewItem(new SimpleProduct("Болт М5", 10));
         thatStorage.addNewItem(new SimpleProduct("Болт", 15));
         thatStorage.addNewItem(new SimpleProduct("Болт", 20));
         thatStorage.addNewItem(new SimpleProduct("Болт", 30));
         thatStorage.addNewItem(new SimpleProduct("Болт", 80));
+        thatStorage.addNewItem(new Article("Гвоздь",
+                "Гвоздь Гвоздь - не Болт не Болт"));
         thatStorage.addNewItem(new SimpleProduct("Болт", 100));
         thatStorage.addNewItem(new SimpleProduct("Болт", 130));
         thatStorage.addNewItem(new SimpleProduct("Болт", 150));
         thatStorage.addNewItem(new Article("Болт",
                 "Болт - не Гвоздь. Гвоздь - не Болт."));
         thatStorage.addNewItem(new DiscountProduct("Гвоздь", 50, 20));
-        thatStorage.addNewItem(new Article("Гвоздь",
-                "Гвоздь Гвоздь - недостаточно гвоздей для критерия BestResult"));
-        thatStorage.addNewItem(new Article("Гвоздь",
-                "Гвоздь Гвоздь Гвоздь Гвоздь Гвоздь - такое многообразие гвоздей в статье введено для демонстрации BestResult"));
+        thatStorage.addNewItem(new Article("Болт",
+                "Гвоздь Гвоздь - не Болт и недостаточно гвоздей для критерия BestResult"));
+        thatStorage.addNewItem(new Article("Молоток",
+                "Гвоздь Гвоздь Гвоздь Гвоздь Гвоздь - забиваем молотком такое многообразие гвоздей в статье для демонстрации BestResult"));
         thatStorage.addNewItem(new FixPriceProduct("Шуруп"));
         thatStorage.addNewItem(new SimpleProduct("Гровер", 100));
         System.out.println(">добавление элементов завершено");
         System.out.println();
 
+        System.out.println("Вывод всего хранилища (метод printAll)");
+        thatStorage.printAll();
+        System.out.println();
 
-        System.out.println(">простой поиск с выводом всех объектов по вхождению в любом поле \"" + searchString + "\" метод searchItem ");
+        System.out.println(">простой поиск в HashMap по ключу \"" + searchString + "\" с выводом всех объектов (метод searchItem)");
         if (!thatStorage.searchItem(searchString).isEmpty()) {
             System.out.println(thatStorage.searchItem(searchString));
         }
         System.out.println();
 
-        System.out.println(">поиск наибольшего количества совпадений по вхождению \"" + searchString + "\" searchBestObject ");
+        System.out.println(">поиск наибольшего количества совпадений по вхождению (в т.ч. в содержимом) \"" + searchString + "\" (метод searchBestObject)");
         try {
             System.out.println(thatStorage.searchBestObject(searchString));
         } catch (BestResultNotFound e) {
@@ -77,6 +78,7 @@ public class App {
     public static void productBasketDemo(String searchString) {
         ProductBasket productBasket = new ProductBasket();
         System.out.println(">РАБОТА С PRODUCTBASKET:");
+        productBasket.printBasket();
         System.out.println(">добавление товаров в корзину, метод addToBasket:");
         productBasket.addToBasket(new SimpleProduct("Шайба", 100));
         productBasket.addToBasket(new FixPriceProduct("Саморез"));
